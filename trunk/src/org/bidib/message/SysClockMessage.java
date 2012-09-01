dff9fb38e7655aa5aef64029ea406500f05744d7
andre_schenk <andre_schenk@87107999-b52a-42af-a34b-1e85ba8a0939>
2012-09-01 13:38:10 +0000

    cosmetics
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@24 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/src/org/bidib/message/SysClockMessage.java
+++ trunk/src/org/bidib/message/SysClockMessage.java
@@ -6,13 +6,18 @@ import org.bidib.BidibLibrary;
 
 public class SysClockMessage extends BidibMessage {
     public SysClockMessage(Calendar date, int factor) {
-        //@formatter:off
-        super(0,
-              BidibLibrary.MSG_SYS_CLOCK,
-              (byte) (date.get(Calendar.MINUTE) & 0x3F),
-              (byte) (date.get(Calendar.HOUR_OF_DAY) & 0x9F),
-              (byte) (((date.get(Calendar.DAY_OF_WEEK) + 5) % 7) & 0x47),
-              (byte) (factor & 0xDF));
-        //@formatter:on
+        super(0, BidibLibrary.MSG_SYS_CLOCK, getMinute(date), getHour(date), getDay(date), (byte) (factor & 0xDF));
+    }
+
+    public static byte getMinute(Calendar date) {
+        return (byte) (date.get(Calendar.MINUTE) & 0x3F);
+    }
+
+    public static byte getHour(Calendar date) {
+        return (byte) (date.get(Calendar.HOUR_OF_DAY) & 0x9F);
+    }
+
+    public static byte getDay(Calendar date) {
+        return (byte) (((date.get(Calendar.DAY_OF_WEEK) + 5) % 7) & 0x47);
     }
 }
