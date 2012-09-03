128f2b666a75c5421b0336c7270dcf64828c9d34
andre_schenk <andre_schenk@87107999-b52a-42af-a34b-1e85ba8a0939>
2012-09-03 13:55:28 +0000

    fixed encoding or parameters for SysClockMessage
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@26 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/src/org/bidib/message/SysClockMessage.java
+++ trunk/src/org/bidib/message/SysClockMessage.java
@@ -6,7 +6,8 @@ import org.bidib.BidibLibrary;
 
 public class SysClockMessage extends BidibMessage {
     public SysClockMessage(Calendar date, int factor) {
-        super(0, BidibLibrary.MSG_SYS_CLOCK, getMinute(date), getHour(date), getDay(date), (byte) (factor & 0xDF));
+        super(0, BidibLibrary.MSG_SYS_CLOCK, getMinute(date), getHour(date), getDay(date),
+                (byte) ((factor | 0xC0) & 0xDF));
     }
 
     public static byte getMinute(Calendar date) {
@@ -14,10 +15,10 @@ public class SysClockMessage extends BidibMessage {
     }
 
     public static byte getHour(Calendar date) {
-        return (byte) (date.get(Calendar.HOUR_OF_DAY) & 0x9F);
+        return (byte) ((date.get(Calendar.HOUR_OF_DAY) | 0x80) & 0x9F);
     }
 
     public static byte getDay(Calendar date) {
-        return (byte) (((date.get(Calendar.DAY_OF_WEEK) + 5) % 7) & 0x47);
+        return (byte) ((((date.get(Calendar.DAY_OF_WEEK) + 5) % 7) | 0x40) & 0x47);
     }
 }
