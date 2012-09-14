9542ba952d7aa8443d5b3940dfc259448b6fa71f
andre_schenk <andre_schenk@87107999-b52a-42af-a34b-1e85ba8a0939>
2012-09-14 09:00:35 +0000

    added method getAccelerationFactor()
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@35 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/src/org/bidib/message/SysClockMessage.java
+++ trunk/src/org/bidib/message/SysClockMessage.java
@@ -7,18 +7,22 @@ import org.bidib.BidibLibrary;
 public class SysClockMessage extends BidibMessage {
     public SysClockMessage(Calendar date, int factor) {
         super(0, BidibLibrary.MSG_SYS_CLOCK, getMinute(date), getHour(date), getDay(date),
-                (byte) ((factor | 0xC0) & 0xDF));
+                getAccelerationFactor(factor));
     }
 
-    public static byte getMinute(Calendar date) {
-        return (byte) (date.get(Calendar.MINUTE) & 0x3F);
+    public static byte getAccelerationFactor(int factor) {
+        return (byte) ((factor | 0xC0) & 0xDF);
+    }
+
+    public static byte getDay(Calendar date) {
+        return (byte) ((((date.get(Calendar.DAY_OF_WEEK) + 5) % 7) | 0x40) & 0x47);
     }
 
     public static byte getHour(Calendar date) {
         return (byte) ((date.get(Calendar.HOUR_OF_DAY) | 0x80) & 0x9F);
     }
 
-    public static byte getDay(Calendar date) {
-        return (byte) ((((date.get(Calendar.DAY_OF_WEEK) + 5) % 7) | 0x40) & 0x47);
+    public static byte getMinute(Calendar date) {
+        return (byte) (date.get(Calendar.MINUTE) & 0x3F);
     }
 }
