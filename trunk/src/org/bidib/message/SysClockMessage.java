e06fc75568d7aef501783d0f542438db59fc3ae3
andre_schenk <andre_schenk@87107999-b52a-42af-a34b-1e85ba8a0939>
2012-09-21 14:00:53 +0000

    use integer values as parameter for getHour() and getMinute()
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@38 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/src/org/bidib/message/SysClockMessage.java
+++ trunk/src/org/bidib/message/SysClockMessage.java
@@ -6,8 +6,8 @@ import org.bidib.BidibLibrary;
 
 public class SysClockMessage extends BidibMessage {
     public SysClockMessage(Calendar date, int factor) {
-        super(0, BidibLibrary.MSG_SYS_CLOCK, getMinute(date), getHour(date), getDay(date),
-                getAccelerationFactor(factor));
+        super(0, BidibLibrary.MSG_SYS_CLOCK, getMinute(date.get(Calendar.MINUTE)), getHour(date
+                .get(Calendar.HOUR_OF_DAY)), getDay(date), getAccelerationFactor(factor));
     }
 
     public static byte getAccelerationFactor(int factor) {
@@ -18,11 +18,11 @@ public class SysClockMessage extends BidibMessage {
         return (byte) ((((date.get(Calendar.DAY_OF_WEEK) + 5) % 7) | 0x40) & 0x47);
     }
 
-    public static byte getHour(Calendar date) {
-        return (byte) ((date.get(Calendar.HOUR_OF_DAY) | 0x80) & 0x9F);
+    public static byte getHour(int hour) {
+        return (byte) ((hour | 0x80) & 0x9F);
     }
 
-    public static byte getMinute(Calendar date) {
-        return (byte) (date.get(Calendar.MINUTE) & 0x3F);
+    public static byte getMinute(int minute) {
+        return (byte) (minute & 0x3F);
     }
 }
