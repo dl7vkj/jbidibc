353074eebe96eb54c2d0b4ea5415dce3ff29418e
andre_schenk <andre_schenk@87107999-b52a-42af-a34b-1e85ba8a0939>
2012-11-23 07:05:16 +0000

    fixed ping
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@70 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/src/org/bidib/message/SysPingMessage.java
+++ trunk/src/org/bidib/message/SysPingMessage.java
@@ -4,6 +4,6 @@ import org.bidib.BidibLibrary;
 
 public class SysPingMessage extends BidibMessage {
     public SysPingMessage() {
-        super(0, BidibLibrary.MSG_SYS_PING, null);
+        super(0, BidibLibrary.MSG_SYS_PING, new byte[] { 1 });
     }
 }
--- trunk/src/org/bidib/message/SysPongResponse.java
+++ trunk/src/org/bidib/message/SysPongResponse.java
@@ -5,7 +5,7 @@ import org.bidib.exception.ProtocolException;
 public class SysPongResponse extends BidibMessage {
     SysPongResponse(byte[] addr, int num, int type, byte... data) throws ProtocolException {
         super(addr, num, type, data);
-        if (data != null && data.length > 0) {
+        if (data == null || data.length != 1) {
             throw new ProtocolException("invalid data in pong response");
         }
     }
