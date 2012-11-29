7db4991c8aa069fe5f3e1fdb76d24992827e46d4
andre_schenk <andre_schenk@87107999-b52a-42af-a34b-1e85ba8a0939>
2012-11-29 12:48:48 +0000

    removed unused inheritance
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@73 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/src/org/bidib/BidibLibrary.java
+++ trunk/src/org/bidib/BidibLibrary.java
@@ -5,7 +5,7 @@ package org.bidib;
  * a tool written by <a href="http://ochafik.com/">Olivier Chafik</a> that <a href="http://code.google.com/p/jnaerator/wiki/CreditsAndLicense">uses a few opensource projects.</a>.<br>
  * For help, please visit <a href="http://nativelibs4java.googlecode.com/">NativeLibs4Java</a> , <a href="http://rococoa.dev.java.net/">Rococoa</a>, or <a href="http://jna.dev.java.net/">JNA</a>.
  */
-public interface BidibLibrary extends com.sun.jna.Library {
+public interface BidibLibrary {
 	/**
 	 * <i>native declaration : bidib_messages.h:33</i><br>
 	 * enum values
