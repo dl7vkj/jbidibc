2a86d119c62b5bf1aa46a42882c18bdf4206cd6f
andre_schenk <andre_schenk@87107999-b52a-42af-a34b-1e85ba8a0939>
2012-12-20 19:18:06 +0000

    get some features from unique id
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@81 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/src/org/bidib/Node.java
+++ trunk/src/org/bidib/Node.java
@@ -35,6 +35,26 @@ public class Node {
         return String.format("0x%014x", (uniqueId & 0xffffffffffffffL));
     }
 
+    public boolean hasAccessoryFunctions() {
+        return ((uniqueId >> 48) & 4) == 4;
+    }
+
+    public boolean hasBoosterFunctions() {
+        return ((uniqueId >> 48) & 2) == 2;
+    }
+
+    public boolean hasFeedbackFunctions() {
+        return ((uniqueId >> 48) & 64) == 64;
+    }
+
+    public boolean hasSubNodesFunctions() {
+        return ((uniqueId >> 48) & 128) == 128;
+    }
+
+    public boolean hasSwitchFunctions() {
+        return ((uniqueId >> 48) & 1) == 1;
+    }
+
     public String toString() {
         return getClass().getSimpleName() + "[version=" + version + ",addr=" + Arrays.toString(addr) + ",uniqueId="
                 + getUniqueIdAsString() + "]";
