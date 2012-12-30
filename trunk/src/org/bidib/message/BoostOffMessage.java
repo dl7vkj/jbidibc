2b7d5bd009dfd8c8fbfda02f135eeb6024b02823
andre_schenk <andre_schenk@87107999-b52a-42af-a34b-1e85ba8a0939>
2012-12-30 10:54:12 +0000

    fixed typo
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@90 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/src/org/bidib/message/BoostOffMessage.java
+++ trunk/src/org/bidib/message/BoostOffMessage.java
@@ -4,6 +4,6 @@ import org.bidib.BidibLibrary;
 
 public class BoostOffMessage extends BidibMessage {
     public BoostOffMessage() {
-        super(0, BidibLibrary.MSG_BOOST_ON, null);
+        super(0, BidibLibrary.MSG_BOOST_OFF, null);
     }
 }
