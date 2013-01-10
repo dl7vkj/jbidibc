9b0469fd4c88421ee558315ce78f2b6089690f3c
andre_schenk <andre_schenk@87107999-b52a-42af-a34b-1e85ba8a0939>
2013-01-10 18:46:18 +0000

    accept day as integer value in method getDay()
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@92 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/src/org/bidib/message/SysClockMessage.java
+++ trunk/src/org/bidib/message/SysClockMessage.java
@@ -7,15 +7,15 @@ import org.bidib.BidibLibrary;
 public class SysClockMessage extends BidibMessage {
     public SysClockMessage(Calendar date, int factor) {
         super(0, BidibLibrary.MSG_SYS_CLOCK, getMinute(date.get(Calendar.MINUTE)), getHour(date
-                .get(Calendar.HOUR_OF_DAY)), getDay(date), getAccelerationFactor(factor));
+                .get(Calendar.HOUR_OF_DAY)), getDay(date.get(Calendar.DAY_OF_WEEK)), getAccelerationFactor(factor));
     }
 
     public static byte getAccelerationFactor(int factor) {
         return (byte) ((factor | 0xC0) & 0xDF);
     }
 
-    public static byte getDay(Calendar date) {
-        return (byte) ((((date.get(Calendar.DAY_OF_WEEK) + 5) % 7) | 0x40) & 0x47);
+    public static byte getDay(int day) {
+        return (byte) ((((day + 5) % 7) | 0x40) & 0x47);
     }
 
     public static byte getHour(int hour) {
