8f3c988661b97b76730c2e9193b9d6d9ac7eae44
akuhtz <akuhtz@87107999-b52a-42af-a34b-1e85ba8a0939>
2013-01-21 20:19:14 +0000

    Added release scripts for build on windows, fixed the path in the <scm>-tag and added the configuration for the maven-release-plugin. See readme.txt on how to perform a release.
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@103 87107999-b52a-42af-a34b-1e85ba8a0939

---
+++ branches/experimental-jbidibc/release/set_properties.cmd
@@ -0,0 +1,22 @@
+set TAG_NAME=1.0
+set NEXT_DEV_VERSION=1.1-SNAPSHOT
+
+set PROJECT_NAME=jbidibc
+
+set SVN_USERNAME=<your-sourceforge-username>
+set SVN_PASSWORD=<your-sourceforge-password>
+
+# the local path to the directory where the release will be performed (clean checkout, build, create tag, build release version)
+SET LOCAL_RELEASE_BASE=C:\release
+
+SET JAVA_HOME=C:\Program Files\Java\jdk1.6.0_32
+SET M2_HOME=C:\tools\apache-maven-3.0.4
+SET ANT_HOME=C:\tools\apache-ant-1.8.4
+SET SVN_HOME=C:\Program Files\TortoiseSVN\Subversion
+
+SET SVN_BASE_PATH=https://svn.code.sf.net/p/jbidibc/code/branches/experimental-jbidibc
+SET SVN_TAGS_PATH=https://svn.code.sf.net/p/jbidibc/code/tags
+
+SET PATH=%M2_HOME%\bin;%JAVA_HOME%\bin;%SVN_HOME%;%ANT_HOME%\bin;%PATH%
+
+set MAVEN_OPTS=-Xmx1024m -XX:MaxPermSize=256m
