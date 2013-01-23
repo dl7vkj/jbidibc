27a7313810279cfc196ffa8a6682c731e09747d9
andre_schenk <andre_schenk@87107999-b52a-42af-a34b-1e85ba8a0939>
2013-01-23 22:24:35 +0000

    some refactoring
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@128 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/jbidibc/jbidibc/src/main/java/org/bidib/jbidibc/message/FwUpdateOpMessage.java
+++ trunk/jbidibc/jbidibc/src/main/java/org/bidib/jbidibc/message/FwUpdateOpMessage.java
@@ -5,16 +5,6 @@ import org.bidib.jbidibc.enumeration.FirmwareUpdateOperation;
 
 public class FwUpdateOpMessage extends BidibMessage {
     public FwUpdateOpMessage(FirmwareUpdateOperation operation, byte... data) {
-        super(0, BidibLibrary.MSG_FW_UPDATE_OP, join(operation.getType(), data));
-    }
-
-    private static byte[] join(byte b, byte[] bytes) {
-        byte[] result = new byte[bytes != null ? bytes.length + 1 : 1];
-
-        result[0] = b;
-        if (bytes != null) {
-            System.arraycopy(bytes, 0, result, 1, bytes.length);
-        }
-        return result;
+        super(0, BidibLibrary.MSG_FW_UPDATE_OP, concat(new byte[] { operation.getType() }, data));
     }
 }
