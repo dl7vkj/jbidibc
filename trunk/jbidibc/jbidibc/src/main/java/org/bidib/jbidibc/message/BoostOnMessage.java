bdaba9a1c16da971a2710be8578aecd998a5f656
andre_schenk <andre_schenk@87107999-b52a-42af-a34b-1e85ba8a0939>
2013-01-25 14:36:27 +0000

    simplified project structure
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@135 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/jbidibc/jbidibc/src/main/java/org/bidib/jbidibc/message/BoostOnMessage.java
+++
@@ -1,9 +0,0 @@
-package org.bidib.jbidibc.message;
-
-import org.bidib.jbidibc.BidibLibrary;
-
-public class BoostOnMessage extends BidibMessage {
-    public BoostOnMessage() {
-        super(0, BidibLibrary.MSG_BOOST_ON);
-    }
-}
--- trunk/jbidibc/jbidibc/src/main/java/org/bidib/jbidibc/message/BoostQueryMessage.java
+++
@@ -1,9 +0,0 @@
-package org.bidib.jbidibc.message;
-
-import org.bidib.jbidibc.BidibLibrary;
-
-public class BoostQueryMessage extends BidibMessage {
-    public BoostQueryMessage() {
-        super(0, BidibLibrary.MSG_BOOST_QUERY);
-    }
-}
