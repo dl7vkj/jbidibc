14f836b82136349964815299e79d936a8cc9bba0
akuhtz <akuhtz@87107999-b52a-42af-a34b-1e85ba8a0939>
2013-01-31 09:40:46 +0000

    Cleanup
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@150 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/jbidibc/src/main/java/org/bidib/jbidibc/message/BidibMessage.java
+++ trunk/jbidibc/src/main/java/org/bidib/jbidibc/message/BidibMessage.java
@@ -69,22 +69,6 @@ public class BidibMessage {
         }
     }
 
-//    protected static byte[] concat(byte[] array1, byte[] array2) {
-//        byte[] result = new byte[array1.length + array2.length];
-//
-//        System.arraycopy(array1, 0, result, 0, array1.length);
-//        System.arraycopy(array2, 0, result, array1.length, array2.length);
-//        return result;
-//    }
-//
-//    public static byte[] convertLongToUniqueId(long uniqueId) {
-//        return BigInteger.valueOf(uniqueId).toByteArray();
-//    }
-//
-//    public static long convertUniqueIdToLong(byte[] uniqueId) {
-//        return new BigInteger(uniqueId).longValue();
-//    }
-//
     public byte[] getAddr() {
         return addr;
     }
@@ -100,21 +84,6 @@ public class BidibMessage {
     public byte[] getData() {
         return data;
     }
-
-//    public static String toString(byte[] bytes) {
-//        StringBuffer result = new StringBuffer("[");
-//
-//        if (bytes != null) {
-//            for (byte b : bytes) {
-//                if (result.length() > 1) {
-//                    result.append(", ");
-//                }
-//                result.append(b & 0xFF);
-//            }
-//        }
-//        result.append("]");
-//        return result.toString();
-//    }
 
     public String toString() {
         return getClass().getSimpleName() + "[" + (addr != null ? ByteUtils.toString(addr) + "," : "") + "num=" + (num & 0xFF)
