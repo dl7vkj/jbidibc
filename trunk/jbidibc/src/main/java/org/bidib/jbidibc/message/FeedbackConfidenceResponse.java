49249b67f50090fc05b73d7e9f1f48ecdc6777d2
akuhtz <akuhtz@87107999-b52a-42af-a34b-1e85ba8a0939>
2013-02-02 20:52:17 +0000

    Log information about confidence response
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@156 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/jbidibc/src/main/java/org/bidib/jbidibc/message/FeedbackConfidenceResponse.java
+++ trunk/jbidibc/src/main/java/org/bidib/jbidibc/message/FeedbackConfidenceResponse.java
@@ -1,13 +1,18 @@
 package org.bidib.jbidibc.message;
 
 import org.bidib.jbidibc.exception.ProtocolException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FeedbackConfidenceResponse extends BidibMessage {
+	private static final Logger LOGGER = LoggerFactory.getLogger(FeedbackConfidenceResponse.class);
+	
     FeedbackConfidenceResponse(byte[] addr, int num, int type, byte... data) throws ProtocolException {
         super(addr, num, type, data);
         if (data == null || data.length != 3) {
             throw new ProtocolException("no feedback confidence received");
         }
+        LOGGER.debug("Received confidence response, valid: {}, freeze: {}, signal: {}", getValid(), getFreeze(), getSignal());
     }
 
     public int getValid() {
@@ -21,4 +26,5 @@ public class FeedbackConfidenceResponse extends BidibMessage {
     public int getSignal() {
         return getData()[2];
     }
+    
 }
