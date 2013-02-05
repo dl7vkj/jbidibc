13050cbc2da803ff3f769ff3fe4df460ad1deca6
akuhtz <akuhtz@87107999-b52a-42af-a34b-1e85ba8a0939>
2013-02-05 21:04:35 +0000

    try to control the lights (see commented out code)
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@166 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/jbidibc/src/main/java/org/bidib/jbidibc/message/CommandStationDriveMessage.java
+++ trunk/jbidibc/src/main/java/org/bidib/jbidibc/message/CommandStationDriveMessage.java
@@ -5,8 +5,12 @@ import java.util.BitSet;
 import org.bidib.jbidibc.BidibLibrary;
 import org.bidib.jbidibc.enumeration.SpeedSteps;
 import org.bidib.jbidibc.utils.ByteUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CommandStationDriveMessage extends BidibMessage {
+	private static final Logger LOGGER = LoggerFactory.getLogger(CommandStationDriveMessage.class);
+	
     public CommandStationDriveMessage(int address, SpeedSteps speedSteps, Integer speed, BitSet activeFunctions,
             BitSet functions) {
         super(0, BidibLibrary.MSG_CS_DRIVE, ByteUtils.concat(new byte[] { (byte) (address & 0xFF),
@@ -42,9 +46,27 @@ public class CommandStationDriveMessage extends BidibMessage {
         int bitIndex = 0;
 
         if (bits != null) {
-            while (bitIndex < bits.length()) {
+        	LOGGER.debug("Prepared bits: {}", bits);
+
+        	// TODO this prepared the first byte correct but didn't put on the lights
+//        	while (bitIndex < bits.length()) {
+//        		if (bitIndex < 8) {
+//        			byte current = result[0];
+//        			for (;bitIndex < 8; bitIndex++ ) {
+//        				current |= (convert(bits.get(bitIndex)) << bitIndex);
+//        				
+//        				LOGGER.debug("Prepared current: {}", current);
+//        			}
+//        			result[0] = current;
+//        		}
+//        		bitIndex++;
+//        	}
+
+        	// TODO this cause an AIOOBEx
+        	while (bitIndex < bits.length()) {
                 if (bitIndex == 0) {
-                    result[4] = convert(bits.get(bitIndex));
+                	boolean bit = bits.get(bitIndex);
+                    result[4] = convert(bit/*bits.get(bitIndex)*/);
                 } else if (bitIndex < 5) {
                     result[bitIndex - 1] = convert(bits.get(bitIndex));
                 } else {
