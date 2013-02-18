e4d986f519839a198ee7eba5bf56e146d57e05a0
akuhtz <akuhtz@87107999-b52a-42af-a34b-1e85ba8a0939>
2013-02-18 13:47:29 +0000

    remove test release
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@200 87107999-b52a-42af-a34b-1e85ba8a0939

--- tags/0.99/jbidibc/src/main/java/org/bidib/jbidibc/message/SysPingMessage.java
+++
@@ -1,9 +0,0 @@
-package org.bidib.jbidibc.message;
-
-import org.bidib.jbidibc.BidibLibrary;
-
-public class SysPingMessage extends BidibMessage {
-    public SysPingMessage() {
-        super(0, BidibLibrary.MSG_SYS_PING, new byte[] { 1 });
-    }
-}
