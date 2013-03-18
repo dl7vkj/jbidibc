4f27d2349cab6fd38c207dad5f5517609940caff
akuhtz <akuhtz@87107999-b52a-42af-a34b-1e85ba8a0939>
2013-03-18 13:42:25 +0000

    reverted javadoc is missing
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@255 87107999-b52a-42af-a34b-1e85ba8a0939

--- tags/1.3/jbidibc/src/main/java/org/bidib/jbidibc/message/LcMacroGetMessage.java
+++
@@ -1,9 +0,0 @@
-package org.bidib.jbidibc.message;
-
-import org.bidib.jbidibc.BidibLibrary;
-
-public class LcMacroGetMessage extends BidibMessage {
-    public LcMacroGetMessage(int macroNumber, int stepNumber) {
-        super(0, BidibLibrary.MSG_LC_MACRO_GET, new byte[] { (byte) macroNumber, (byte) stepNumber });
-    }
-}
