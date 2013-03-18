12817c92b3b9714385891748460b7292034ce503
akuhtz <akuhtz@87107999-b52a-42af-a34b-1e85ba8a0939>
2013-03-18 13:42:43 +0000

    reverted javadoc is missing
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@256 87107999-b52a-42af-a34b-1e85ba8a0939

--- tags/1.3.old/jbidibc/src/main/java/org/bidib/jbidibc/message/VendorEnableMessage.java
+++
@@ -1,9 +0,0 @@
-package org.bidib.jbidibc.message;
-
-import org.bidib.jbidibc.BidibLibrary;
-
-public class VendorEnableMessage extends BidibMessage {
-    public VendorEnableMessage(byte[] uniqueId) {
-        super(0, BidibLibrary.MSG_VENDOR_ENABLE, uniqueId);
-    }
-}
