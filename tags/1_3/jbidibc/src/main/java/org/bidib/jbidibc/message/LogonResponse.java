e2c82ed5e9494843f7b3669d1701a73605195b72
andre_schenk <andre_schenk@87107999-b52a-42af-a34b-1e85ba8a0939>
2013-03-18 15:31:50 +0000

    removed duplicate release tag
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@260 87107999-b52a-42af-a34b-1e85ba8a0939

--- tags/1_3/jbidibc/src/main/java/org/bidib/jbidibc/message/LogonResponse.java
+++
@@ -1,9 +0,0 @@
-package org.bidib.jbidibc.message;
-
-import org.bidib.jbidibc.exception.ProtocolException;
-
-public class LogonResponse extends SysUniqueIdResponse {
-    LogonResponse(byte[] addr, int num, int type, byte... data) throws ProtocolException {
-        super(addr, num, type, data);
-    }
-}
--- tags/1_3/jbidibc/src/main/java/org/bidib/jbidibc/message/SysResetMessage.java
+++
@@ -1,9 +0,0 @@
-package org.bidib.jbidibc.message;
-
-import org.bidib.jbidibc.BidibLibrary;
-
-public class SysResetMessage extends BidibMessage {
-    public SysResetMessage() {
-        super(0, BidibLibrary.MSG_SYS_RESET);
-    }
-}
