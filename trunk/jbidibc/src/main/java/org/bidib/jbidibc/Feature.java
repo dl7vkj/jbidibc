a44ec9d1c6d0bfdf498503d3c8b90e20acc1d7ba
akuhtz <akuhtz@87107999-b52a-42af-a34b-1e85ba8a0939>
2013-05-22 16:47:00 +0000

    The feature value can be changed now. Added equals and hashCode based on feature type.
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@316 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/jbidibc/src/main/java/org/bidib/jbidibc/Feature.java
+++ trunk/jbidibc/src/main/java/org/bidib/jbidibc/Feature.java
@@ -5,7 +5,7 @@ import org.bidib.jbidibc.enumeration.FeatureEnum;
 public class Feature {
     private final int type;
 
-    private final int value;
+    private int value;
 
     public Feature(int type, int value) {
         this.type = type;
@@ -27,6 +27,23 @@ public class Feature {
 
     public int getValue() {
         return value;
+    }
+    
+    public void setValue(int value) {
+        this.value = value;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Feature) {
+            return getType() == ((Feature)obj).getType();
+        }
+        return false;
+    }
+    
+    @Override
+    public int hashCode() {
+        return getType();
     }
 
     public String toString() {
