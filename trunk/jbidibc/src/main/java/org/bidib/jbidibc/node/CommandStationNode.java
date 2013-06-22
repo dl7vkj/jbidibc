58f03952286627b3ab050c15aa2a24540719e72c
akuhtz <akuhtz@87107999-b52a-42af-a34b-1e85ba8a0939>
2013-06-22 11:39:10 +0000

    Add support to release a loco from locobuffer (used to clean locobuffer at startup)
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@335 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/jbidibc/src/main/java/org/bidib/jbidibc/node/CommandStationNode.java
+++ trunk/jbidibc/src/main/java/org/bidib/jbidibc/node/CommandStationNode.java
@@ -75,4 +75,20 @@ public class CommandStationNode {
         }
         return result;
     }
+
+    public DriveAcknowledge releaseLoco(
+        int address) throws IOException, ProtocolException, InterruptedException {
+
+        LOGGER.debug("Release loco, address: {}", address);
+
+        DriveAcknowledge result = null;
+        BidibMessage response =
+            delegate.send(new CommandStationDriveMessage(address, SpeedStepsEnum.DCC128, null, DirectionEnum.BACKWARD, null,
+                null), true, CommandStationDriveAcknowledgeResponse.TYPE);
+
+        if (response instanceof CommandStationDriveAcknowledgeResponse) {
+            result = ((CommandStationDriveAcknowledgeResponse) response).getState();
+        }
+        return result;
+    }
 }
