b30e56f2a6072713721e4dd633a998a2b38e079e
akuhtz <akuhtz@87107999-b52a-42af-a34b-1e85ba8a0939>
2013-09-08 15:25:45 +0000

    [#20] Prepared vendor_cv.xslt to read the VendorCV-file for light control, S88-bridge and OneOC that is validated against vendor_cv.xsd
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@429 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/jbidibc-exchange/src/test/java/org/bidib/jbidibc/vendorcv/VendorCVTest.java
+++ trunk/jbidibc-exchange/src/test/java/org/bidib/jbidibc/vendorcv/VendorCVTest.java
@@ -6,11 +6,17 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.util.JAXBResult;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
@@ -43,15 +49,58 @@ public class VendorCVTest {
     }
 
     @Test
-    public void loadVendorCVTest() throws JAXBException {
+    public void loadVendorCVForLCAndTransformToWizardFormatTest() throws JAXBException, TransformerException,
+        IOException {
+        LOGGER.info("Load VendorCV for LC and transform to wizard data.");
+
+        System.setProperty("javax.xml.transform.TransformerFactory", "net.sf.saxon.TransformerFactoryImpl");
+
+        InputStream xsltStream = VendorCVTest.class.getResourceAsStream("/xsd/vendor_cv.xslt");
+        javax.xml.transform.Source xsltSource = new javax.xml.transform.stream.StreamSource(xsltStream);
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer(xsltSource);
+
+        InputStream is = VendorCVTest.class.getResourceAsStream("/xsd/05.00.0D.6B.00-VendorCV.xml");
+        StreamSource xmlSource = new StreamSource(is);
+
+        /*
+         * Perform the transform to get the report
+         */
+        File exportFile = new File(EXPORTED_CVDEF_TARGET_DIR, "vendorcv_wizard_LC.xml");
+
+        FileOutputStream reportFOS = new FileOutputStream(exportFile);
+        OutputStreamWriter osw = new OutputStreamWriter(reportFOS, "UTF-8");
+        transformer.transform(xmlSource, new StreamResult(osw));
+        osw.close();
+
+        LOGGER.info("Prepared wizard xml file: {}", exportFile.getPath());
+    }
+
+    @Test
+    public void loadVendorForLCAndTransformCVTest() throws JAXBException, TransformerException {
         LOGGER.info("Load VendorCV.");
+
+        System.setProperty("javax.xml.transform.TransformerFactory", "net.sf.saxon.TransformerFactoryImpl");
+
+        InputStream xsltStream = VendorCVTest.class.getResourceAsStream("/xsd/vendor_cv.xslt");
+        javax.xml.transform.Source xsltSource = new javax.xml.transform.stream.StreamSource(xsltStream);
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer(xsltSource);
+
+        InputStream is = VendorCVTest.class.getResourceAsStream("/xsd/05.00.0D.6B.00-VendorCV.xml");
+        StreamSource xmlSource = new StreamSource(is);
 
         JAXBContext jaxbContext = JAXBContext.newInstance(JAXB_PACKAGE);
 
-        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        JAXBResult outputTarget = new JAXBResult(jaxbContext);
+        transformer.transform(xmlSource, outputTarget);
 
-        InputStream is = VendorCVTest.class.getResourceAsStream("/xsd/05.00.0D.6B.00-VendorCV.xml");
-        VendorCV vendorCV = (VendorCV) unmarshaller.unmarshal(is);
+        LOGGER.info("Prepared outputTarget: {}", outputTarget);
+
+        //        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        //
+        //        VendorCV vendorCV = (VendorCV) unmarshaller.unmarshal(is);
+        VendorCV vendorCV = (VendorCV) outputTarget.getResult();
         Assert.assertNotNull(vendorCV);
 
         Assert.assertNotNull(vendorCV.getVersion());
@@ -93,7 +142,92 @@ public class VendorCVTest {
     }
 
     @Test
-    public void saveVendorCVTest() throws JAXBException, SAXException, IOException {
+    public void loadVendorCVForS88BridgeAndTransformToWizardFormatTest() throws JAXBException, TransformerException,
+        IOException {
+        LOGGER.info("Load VendorCV for S88Bridge and transform to wizard data.");
+
+        System.setProperty("javax.xml.transform.TransformerFactory", "net.sf.saxon.TransformerFactoryImpl");
+
+        InputStream xsltStream = VendorCVTest.class.getResourceAsStream("/xsd/vendor_cv.xslt");
+        javax.xml.transform.Source xsltSource = new javax.xml.transform.stream.StreamSource(xsltStream);
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer(xsltSource);
+        //        transformer.setOutputProperty(OutputKeys.INDENT, "no");
+
+        InputStream is = VendorCVTest.class.getResourceAsStream("/xsd/monitor-0.4.2.3/40.00.0D.69.00-VendorCV.xml");
+        StreamSource xmlSource = new StreamSource(is);
+
+        /*
+         * Perform the transform to get the report
+         */
+        File exportFile = new File(EXPORTED_CVDEF_TARGET_DIR, "vendorcv_wizard_S88Bridge.xml");
+
+        FileOutputStream reportFOS = new FileOutputStream(exportFile);
+        OutputStreamWriter osw = new OutputStreamWriter(reportFOS, "UTF-8");
+        transformer.transform(xmlSource, new StreamResult(osw));
+        osw.close();
+
+        LOGGER.info("Prepared wizard xml file: {}", exportFile.getPath());
+    }
+
+    @Test
+    public void saveVendorCVS88BridgeTest() throws JAXBException, SAXException, IOException {
+        LOGGER.info("Save vendor CV for S88Bridge.");
+        JAXBContext jaxbContext = JAXBContext.newInstance(JAXB_PACKAGE);
+
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller.setProperty(Marshaller.JAXB_SCHEMA_LOCATION, XSD_LOCATION);
+
+        VersionInfoType versionInfo = new VersionInfoType();
+        versionInfo.setAuthor("Max Mustermann");
+        versionInfo.setVersion("0.1");
+        versionInfo.setVendor("013");
+
+        VendorCV vendorCV = new VendorCV();
+        vendorCV.setVersion(versionInfo);
+
+        CVDefinitionType cvDefinition = new CVDefinitionType();
+        Basis basis = new Basis();
+        CV cv = new CV();
+        cv.setNumber(1);
+        cv.setType(DataType.BYTE);
+        cv.setMin("0");
+        cv.setMax("255");
+        cv.setLow("-");
+        cv.setHigh("-");
+        cv.setValues("-");
+        cv.setMode(ModeType.RO);
+        cv.setDescde("vendor_id");
+        cv.setDescen("vendor_id");
+        basis.getCV().add(cv);
+
+        cv = new CV();
+        cv.setNumber(2);
+        cv.setType(DataType.BYTE);
+        cv.setMin("0");
+        cv.setMax("255");
+        cv.setLow("-");
+        cv.setHigh("-");
+        cv.setValues("-");
+        cv.setMode(ModeType.RO);
+        cv.setDescde("hw_id");
+        cv.setDescen("hw_id");
+        basis.getCV().add(cv);
+
+        cvDefinition.setBasis(basis);
+        vendorCV.setCVDefinition(cvDefinition);
+
+        File exportFile = new File(EXPORTED_CVDEF_TARGET_DIR, "vendorcv_S88Bridge.xml");
+        OutputStream os = new BufferedOutputStream(new FileOutputStream(exportFile));
+
+        marshaller.marshal(vendorCV, os);
+
+        os.flush();
+    }
+
+    @Test
+    public void saveVendorCVLCTest() throws JAXBException, SAXException, IOException {
 
         JAXBContext jaxbContext = JAXBContext.newInstance(JAXB_PACKAGE);
 
--- trunk/jbidibc-exchange/src/test/resources/xsd/vendor_cv.xslt
+++ trunk/jbidibc-exchange/src/test/resources/xsd/vendor_cv.xslt
@@ -1,63 +1,94 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet version="2.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:vendorcv="http://www.bidib.org/vendorcv" exclude-result-prefixes="#default"  >
-	<xsl:output method="xml" version="1.0" encoding="UTF-8" indent="yes"/>
-	<xsl:strip-space elements="*"/>
-	
-	<!-- string for default namespace uri and schema location -->
-	<xsl:variable name="nsVendorCV" select="'http://www.bidib.org/vendorcv'"/>
-    <xsl:variable name="schemaLoc" select="'http://www.bidib.org/vendorcv vendor_cv.xsd'"/>
-	
-	<xsl:template match="@*|node()">
-        <xsl:copy inherit-namespaces="yes">
-            <xsl:apply-templates select="@*|node()"/>
-        </xsl:copy>
-    </xsl:template>
+<xsl:stylesheet version="2.0"
+	xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:xs="http://www.w3.org/2001/XMLSchema"
+	xmlns:vendorcv="http://www.bidib.org/vendorcv" exclude-result-prefixes="#default" xmlns="http://www.bidib.org/vendorcv">
+	<xsl:output method="xml" version="1.0" encoding="UTF-8"
+		indent="yes" />
+	<xsl:strip-space elements="*" />
 
-    <xsl:template match="/*" priority="1">
-        <xsl:element name="{local-name()}" namespace="{$nsVendorCV}">
+	<!-- string for default namespace uri and schema location -->
+	<xsl:variable name="nsVendorCV" select="'http://www.bidib.org/vendorcv'" />
+	<xsl:variable name="schemaLoc"
+		select="'http://www.bidib.org/vendorcv vendor_cv.xsd'" />
+
+	<xsl:template match="@*|node()">
+		<xsl:copy inherit-namespaces="yes">
+			<xsl:apply-templates select="@*|node()" />
+		</xsl:copy>
+	</xsl:template>
+
+	<xsl:template match="/*" priority="1">
+		<xsl:element name="{local-name()}" namespace="{$nsVendorCV}">
 			<xsl:attribute name="xsi:schemaLocation"
 				namespace="http://www.w3.org/2001/XMLSchema-instance">
-				<xsl:value-of select="$schemaLoc"/>
+				<xsl:value-of select="$schemaLoc" />
 			</xsl:attribute>
-            <xsl:namespace name="xsi" select="'http://www.w3.org/2001/XMLSchema-instance'"/>
-            <xsl:apply-templates select="@*|node()"/>
-        </xsl:element>
-    </xsl:template>
+			<xsl:namespace name="xsi"
+				select="'http://www.w3.org/2001/XMLSchema-instance'" />
+			<xsl:apply-templates select="@*|node()" />
+		</xsl:element>
+	</xsl:template>
 
-    <!--template for elements without a namespace -->
-  <xsl:template match="*[namespace-uri() = '']">
-    <xsl:element name="{local-name()}" namespace="{$nsVendorCV}">
-      <xsl:apply-templates select="@* | node()"/>
-    </xsl:element>
-  </xsl:template>
-	
-	<!--
-    <xsl:template match="*" priority="2">
-        <xsl:element name="{local-name()}" namespace="{$nsVendorCV}" exclude-result-prefixes="#default">
-            <xsl:apply-templates select="@*|node()"/>
-        </xsl:element>
-    </xsl:template>
-    -->
-    
+	<!--template for elements without a namespace -->
+	<xsl:template match="*[namespace-uri() = '']">
+		<xsl:element name="{local-name()}" namespace="{$nsVendorCV}">
+			<xsl:apply-templates select="@* | node()" />
+		</xsl:element>
+	</xsl:template>
+
+	<!-- support format of monitor-0.4.2.3 -->
+	<xsl:template match="CV[not(@number)]" priority="10">
+		<xsl:element name="{name()}" namespace="{$nsVendorCV}"
+			exclude-result-prefixes="#default">
+			<xsl:for-each select="./*">
+				<xsl:attribute name="{local-name()}" >
+					<xsl:value-of
+						select="node()" />
+				</xsl:attribute>
+			</xsl:for-each>
+		</xsl:element>
+	</xsl:template>
+
 	<xsl:template match="LEDS" priority="10">
-		<xsl:element name="{name()}" namespace="{$nsVendorCV}" exclude-result-prefixes="#default">
-		<xsl:for-each select="*">
-			<xsl:element name="LedDefinition" namespace="{$nsVendorCV}" exclude-result-prefixes="#default">
-			<xsl:attribute name="number"><xsl:value-of select="substring(current()/local-name(),4)" /></xsl:attribute>
-				<xsl:apply-templates select="./*" />
-			</xsl:element>
-		</xsl:for-each>
+		<xsl:element name="{name()}" namespace="{$nsVendorCV}"
+			exclude-result-prefixes="#default">
+			<xsl:for-each select="*">
+				<xsl:element name="LedDefinition" namespace="{$nsVendorCV}"
+					exclude-result-prefixes="#default">
+					<xsl:attribute name="number"><xsl:value-of
+						select="substring(current()/local-name(),4)" /></xsl:attribute>
+					<xsl:apply-templates select="./*" />
+				</xsl:element>
+			</xsl:for-each>
 		</xsl:element>
 	</xsl:template>
 	<xsl:template match="Servos" priority="10">
-		<xsl:element name="{name()}" namespace="{$nsVendorCV}" exclude-result-prefixes="#default">
-		<xsl:for-each select="*">
-			<!--b><xsl:value-of select="current()/local-name()" /></b-->
-			<xsl:element name="ServoDefinition" namespace="{$nsVendorCV}" exclude-result-prefixes="#default">
-			<xsl:attribute name="number"><xsl:value-of select="substring(current()/local-name(),6)" /></xsl:attribute>
+		<xsl:element name="{name()}" namespace="{$nsVendorCV}"
+			exclude-result-prefixes="#default">
+			<xsl:for-each select="*">
+				<xsl:element name="ServoDefinition" namespace="{$nsVendorCV}"
+					exclude-result-prefixes="#default">
+					<xsl:attribute name="number"><xsl:value-of
+						select="substring(current()/local-name(),6)" /></xsl:attribute>
+					<xsl:apply-templates select="./*" />
+				</xsl:element>
+			</xsl:for-each>
+		</xsl:element>
+	</xsl:template>
+	<xsl:template match="CVDefinition/*" priority="9">
+		<xsl:if test=".[not(starts-with(name(), 'Sector'))]">
+			<xsl:element name="{name()}" namespace="{$nsVendorCV}"
+				exclude-result-prefixes="#default">
+					<xsl:apply-templates select="@* | node()" />
+			</xsl:element>
+		</xsl:if>
+		<xsl:if test=".[starts-with(name(), 'Sector')]">
+			<xsl:element name="SectorDefinition" namespace="{$nsVendorCV}"
+				exclude-result-prefixes="#default">
+				<xsl:attribute name="number"><xsl:value-of
+					select="substring(current()/local-name(),7)" /></xsl:attribute>
 				<xsl:apply-templates select="./*" />
 			</xsl:element>
-		</xsl:for-each>
-		</xsl:element>
+		</xsl:if>
 	</xsl:template>
 </xsl:stylesheet>
