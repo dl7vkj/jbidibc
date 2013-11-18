c3e03cc7d1abd61cc196609982651c571ad1b5ae
akuhtz <akuhtz@87107999-b52a-42af-a34b-1e85ba8a0939>
2013-11-18 10:42:56 +0000

    Failure during upload to repo
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@560 87107999-b52a-42af-a34b-1e85ba8a0939

--- tags/1.5.2/jbidibc/src/main/java/org/bidib/jbidibc/message/NodeChangedAckMessage.java
+++
@@ -1,9 +0,0 @@
-package org.bidib.jbidibc.message;
-
-import org.bidib.jbidibc.BidibLibrary;
-
-public class NodeChangedAckMessage extends BidibMessage {
-    public NodeChangedAckMessage(int versionNumber) {
-        super(0, BidibLibrary.MSG_NODE_CHANGED_ACK, new byte[] { (byte) versionNumber });
-    }
-}
--- tags/1.5.2/jbidibc/src/main/java/org/bidib/jbidibc/message/SysUniqueIdMessage.java
+++
@@ -1,9 +0,0 @@
-package org.bidib.jbidibc.message;
-
-import org.bidib.jbidibc.BidibLibrary;
-
-public class SysUniqueIdMessage extends BidibMessage {
-    public SysUniqueIdMessage() {
-        super(0, BidibLibrary.MSG_SYS_GET_UNIQUE_ID);
-    }
-}
