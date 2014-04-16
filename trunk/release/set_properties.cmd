3c43be8ed5337fccc7823f383348463c478081f8
akuhtz <akuhtz@87107999-b52a-42af-a34b-1e85ba8a0939>
2014-04-16 18:25:11 +0000

    Released jbidibc-1.6.0
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@753 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/release/set_properties.cmd
+++ trunk/release/set_properties.cmd
@@ -1,4 +1,4 @@
-set TAG_NAME=1.5.2
+set TAG_NAME=1.6.0
 set NEXT_DEV_VERSION=1.6-SNAPSHOT
 
 set PROJECT_NAME=jbidibc
@@ -10,10 +10,10 @@ set GPG_PASSPHRASE=<your-gpg-passphrase>
 rem the local path to the directory where the release will be performed (clean checkout, build, create tag, build release version)
 SET LOCAL_RELEASE_BASE=D:\release
 
-SET JAVA_HOME=C:\Program Files\Java\jdk1.6.0_45
-SET M2_HOME=D:\tools\apache-maven-3.0.5
+SET JAVA_HOME=C:\Program Files\Java\jdk1.7.0_25
+SET M2_HOME=D:\tools\apache-maven-3.0.4
 SET ANT_HOME=D:\tools\apache-ant-1.8.1
-SET SVN_HOME=D:\tools\svn-win32-1.8.4
+SET SVN_HOME=D:\tools\svn-win64-1.7.8
 
 SET SVN_BASE_PATH=https://svn.code.sf.net/p/jbidibc/code/trunk
 SET SVN_TAGS_PATH=https://svn.code.sf.net/p/jbidibc/code/tags
