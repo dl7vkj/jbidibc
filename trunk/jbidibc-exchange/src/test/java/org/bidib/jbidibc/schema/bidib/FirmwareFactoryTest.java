783b5ae676f21b25ff0a7764ad153c390d89fc86
akuhtz <akuhtz@87107999-b52a-42af-a34b-1e85ba8a0939>
2014-04-17 11:24:21 +0000

    [#52] Added more tests.
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@758 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/jbidibc-exchange/src/test/java/org/bidib/jbidibc/schema/bidib/FirmwareFactoryTest.java
+++ trunk/jbidibc-exchange/src/test/java/org/bidib/jbidibc/schema/bidib/FirmwareFactoryTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.xml.bind.JAXBException;
 import javax.xml.stream.XMLStreamException;
 
+import org.bidib.jbidibc.schema.bidib.FirmwareFactory.NodetextUtils;
 import org.bidib.schema.firmware.DeviceNode;
 import org.bidib.schema.firmware.Firmware;
 import org.bidib.schema.firmware.FirmwareDefinitionType;
@@ -40,11 +41,92 @@ public class FirmwareFactoryTest {
         NodeType node0 = nodes.get(0);
         Assert.assertNotNull(node0);
         Assert.assertTrue(node0 instanceof DeviceNode);
-        Assert.assertEquals(node0.getNodetext().size(), 2);
-        Assert.assertEquals(node0.getNode().size(), 3);
+
+        DeviceNode deviceNode = (DeviceNode) node0;
+        Assert.assertEquals(deviceNode.getNodetext().size(), 2);
+        Assert.assertEquals(deviceNode.getNode().size(), 3);
+        Assert.assertEquals(deviceNode.getVID(), "013");
+        Assert.assertEquals(deviceNode.getPID(), "201");
+        // we expect comment
+        Assert.assertNotNull(deviceNode.getComment(), "Comment expected!");
 
         Assert.assertTrue(node0.getNode().get(0) instanceof FirmwareNode);
         Assert.assertTrue(node0.getNode().get(1) instanceof FirmwareNode);
         Assert.assertTrue(node0.getNode().get(2) instanceof SimpleNode);
+    }
+
+    @Test
+    public void loadFirmwareDefinitionMinimum() throws JAXBException, SAXException, XMLStreamException {
+        LOGGER.info("Prepare the JAXBContext to load the firmware defintion file.");
+
+        URL url = ProtocolTest.class.getResource("/xml-test/firmware-minimum.xml");
+        Firmware firmware = FirmwareFactory.getFirmware(url.getPath());
+        Assert.assertNotNull(firmware);
+
+        FirmwareDefinitionType firmwareDefinition = firmware.getFirmwareDefinition();
+        Assert.assertNotNull(firmwareDefinition);
+
+        Assert.assertNotNull(firmwareDefinition.getNode());
+
+        List<NodeType> nodes = firmwareDefinition.getNode();
+
+        NodeType node0 = nodes.get(0);
+        Assert.assertNotNull(node0);
+        Assert.assertTrue(node0 instanceof DeviceNode);
+
+        DeviceNode deviceNode = (DeviceNode) node0;
+        Assert.assertEquals(deviceNode.getNodetext().size(), 2);
+        Assert.assertEquals(deviceNode.getNode().size(), 3);
+        Assert.assertEquals(deviceNode.getVID(), "013");
+        Assert.assertEquals(deviceNode.getPID(), "201");
+        Assert.assertNull(deviceNode.getComment(), "No comment expected!");
+
+        Assert.assertTrue(node0.getNode().get(0) instanceof FirmwareNode);
+        Assert.assertTrue(node0.getNode().get(1) instanceof FirmwareNode);
+        Assert.assertTrue(node0.getNode().get(2) instanceof SimpleNode);
+    }
+
+    @Test
+    public void loadFirmwareDefinitionBasic() throws JAXBException, SAXException, XMLStreamException {
+        LOGGER.info("Prepare the JAXBContext to load the firmware defintion file.");
+
+        URL url = ProtocolTest.class.getResource("/xml-test/firmware-basic.xml");
+        Firmware firmware = FirmwareFactory.getFirmware(url.getPath());
+        Assert.assertNotNull(firmware);
+
+        FirmwareDefinitionType firmwareDefinition = firmware.getFirmwareDefinition();
+        Assert.assertNotNull(firmwareDefinition);
+
+        Assert.assertNotNull(firmwareDefinition.getNode());
+
+        List<NodeType> nodes = firmwareDefinition.getNode();
+        Assert.assertNotNull(nodes);
+        Assert.assertEquals(nodes.size(), 2);
+
+        NodeType node0 = nodes.get(0);
+        Assert.assertNotNull(node0);
+        Assert.assertTrue(node0 instanceof DeviceNode);
+
+        DeviceNode deviceNode = (DeviceNode) node0;
+        Assert.assertEquals(deviceNode.getNodetext().size(), 2);
+
+        Assert.assertEquals(NodetextUtils.getText(deviceNode.getNodetext(), "de-DE"), "StepControl: Neuinstallation");
+        Assert.assertEquals(NodetextUtils.getText(deviceNode.getNodetext(), "en-EN"),
+            "StepControl: Initial installation");
+
+        Assert.assertEquals(deviceNode.getNode().size(), 3);
+        Assert.assertEquals(deviceNode.getVID(), "013");
+        Assert.assertEquals(deviceNode.getPID(), "201");
+        Assert.assertNull(deviceNode.getComment(), "No comment expected!");
+
+        // get the update node
+        deviceNode = (DeviceNode) nodes.get(1);
+        Assert.assertEquals(deviceNode.getNodetext().size(), 2);
+
+        Assert.assertEquals(NodetextUtils.getText(deviceNode.getNodetext(), "de-DE"),
+            "StepControl: Update Installation");
+        Assert.assertEquals(NodetextUtils.getText(deviceNode.getNodetext(), "en-EN"),
+            "StepControl: Update installation");
+
     }
 }
