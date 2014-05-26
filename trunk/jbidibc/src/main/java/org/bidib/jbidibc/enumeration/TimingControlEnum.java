40c0c6a0fda4310627a85ed85429f9e3752c25a9
akuhtz <akuhtz@87107999-b52a-42af-a34b-1e85ba8a0939>
2014-05-26 19:17:49 +0000

    [#54] Add support for MSG_CS_ACCESSORY.
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@769 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/jbidibc/src/main/java/org/bidib/jbidibc/enumeration/TimingControlEnum.java
+++ trunk/jbidibc/src/main/java/org/bidib/jbidibc/enumeration/TimingControlEnum.java
@@ -2,13 +2,19 @@ package org.bidib.jbidibc.enumeration;
 
 public enum TimingControlEnum implements BidibEnum {
     // @formatter:off
-    COIL_ON_OFF(0), OUTPUT_UNIT(1);
+    COIL_ON_OFF("coil-on-off", 0), OUTPUT_UNIT("output-unit", 1);
     // @formatter:on
+    private final String key;
 
     private final byte type;
 
-    TimingControlEnum(int type) {
+    TimingControlEnum(String key, int type) {
+        this.key = key;
         this.type = (byte) type;
+    }
+
+    public String getKey() {
+        return key;
     }
 
     public byte getType() {
