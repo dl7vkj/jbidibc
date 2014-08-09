0249ce0f26cf36d9103702150f6c36363eb73b14
akuhtz <akuhtz@87107999-b52a-42af-a34b-1e85ba8a0939>
2014-08-09 10:15:13 +0000

    Check for null value.
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@831 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/jbidibc/src/main/java/org/bidib/jbidibc/enumeration/CommandStationState.java
+++ trunk/jbidibc/src/main/java/org/bidib/jbidibc/enumeration/CommandStationState.java
@@ -1,6 +1,8 @@
 package org.bidib.jbidibc.enumeration;
 
 import org.bidib.jbidibc.BidibLibrary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public enum CommandStationState implements BidibEnum {
     // @formatter:off
@@ -10,6 +12,8 @@ public enum CommandStationState implements BidibEnum {
         BidibLibrary.BIDIB_CS_STATE_BUSY), QUERY(BidibLibrary.BIDIB_CS_STATE_QUERY), GO_IGN_WD(
         BidibLibrary.BIDIB_CS_STATE_GO_IGN_WD);
     // @formatter:on
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommandStationState.class);
 
     private final byte type;
 
@@ -50,6 +54,12 @@ public enum CommandStationState implements BidibEnum {
     }
 
     public static boolean isOffState(CommandStationState commandStationState) {
+        if (commandStationState == null) {
+            LOGGER.warn("Cannot check if command station if off because the commandStationState is null.");
+            throw new IllegalArgumentException(
+                "Cannot check if command station if off because the commandStationState is null.");
+        }
+
         boolean isOffState = false;
         switch (commandStationState) {
             case OFF:
