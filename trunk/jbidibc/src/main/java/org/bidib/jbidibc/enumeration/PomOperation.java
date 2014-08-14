5dd226b9eb128b455310f7d4a5a8ed9ccc1d8dbd
akuhtz <akuhtz@87107999-b52a-42af-a34b-1e85ba8a0939>
2014-08-14 19:32:41 +0000

    [#45] Added revised xPOM.
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@838 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/jbidibc/src/main/java/org/bidib/jbidibc/enumeration/PomOperation.java
+++ trunk/jbidibc/src/main/java/org/bidib/jbidibc/enumeration/PomOperation.java
@@ -1,9 +1,15 @@
 package org.bidib.jbidibc.enumeration;
 
+import org.bidib.jbidibc.BidibLibrary;
+
 public enum PomOperation implements BidibEnum {
     // @formatter:off
     RD_BLOCK(0, "read-block"), RD_BYTE(1, "read-byte"), RD_BIT(1, "read-bit"), WR_BIT(2, "write-bit"), WR_BYTE(3,
-        "write-byte");
+        "write-byte"), X_RD_BLOCK(BidibLibrary.BIDIB_CS_xPOM_RD_BLOCK, "x-read-block"), X_WR_BIT(
+        BidibLibrary.BIDIB_CS_xPOM_WR_BIT, "x-write-bit"), X_WR_BYTE1(BidibLibrary.BIDIB_CS_xPOM_WR_BYTE1,
+        "x-write-byte1"), X_WR_BYTE2(BidibLibrary.BIDIB_CS_xPOM_WR_BYTE2, "x-write-byte2"), X_WR_BYTE3(
+        BidibLibrary.BIDIB_CS_xPOM_WR_BYTE3, "x-write-byte3"), X_WR_BYTE4(BidibLibrary.BIDIB_CS_xPOM_WR_BYTE4,
+        "x-write-byte4");
     // @formatter:on
 
     // NOTE: POM does not support READ-BIT but we use it as operation. In this case the OPCODE of READ-BYTE
