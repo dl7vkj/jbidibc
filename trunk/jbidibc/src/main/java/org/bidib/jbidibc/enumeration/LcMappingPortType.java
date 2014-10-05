e2c54c414f9c44cb4a6dbf1871c0a8c908f03d45
akuhtz <akuhtz@87107999-b52a-42af-a34b-1e85ba8a0939>
2014-10-05 12:12:27 +0000

    Removed FEEDBACKPORT(0xFF), this is not an official value.
    git-svn-id: http://svn.code.sf.net/p/jbidibc/code@878 87107999-b52a-42af-a34b-1e85ba8a0939

--- trunk/jbidibc/src/main/java/org/bidib/jbidibc/enumeration/LcMappingPortType.java
+++ trunk/jbidibc/src/main/java/org/bidib/jbidibc/enumeration/LcMappingPortType.java
@@ -7,7 +7,7 @@ public enum LcMappingPortType implements BidibEnum {
     SWITCHPORT(BidibLibrary.BIDIB_OUTTYPE_SPORT), LIGHTPORT(BidibLibrary.BIDIB_OUTTYPE_LPORT), SERVOPORT(
         BidibLibrary.BIDIB_OUTTYPE_SERVO), SOUNDPORT(BidibLibrary.BIDIB_OUTTYPE_SOUND), MOTORPORT(
         BidibLibrary.BIDIB_OUTTYPE_MOTOR), ANALOGPORT(BidibLibrary.BIDIB_OUTTYPE_ANALOG), BACKLIGHTPORT(
-        BidibLibrary.BIDIB_OUTTYPE_BACKLIGHT), INPUTPORT(BidibLibrary.BIDIB_PORTTYPE_INPUT), FEEDBACKPORT(0xFF);
+        BidibLibrary.BIDIB_OUTTYPE_BACKLIGHT), INPUTPORT(BidibLibrary.BIDIB_PORTTYPE_INPUT);
     // @formatter:on
 
     private final byte type;
